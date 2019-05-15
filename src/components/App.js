0e3b41a1c7e44c37c8aef1e5df97b4b8f9835726
Philip Kloc <philipgkloc@gmail.com>
2019-05-15 11:59:35 -0600

    Add long term debt

--- src/components/App.js
+++ src/components/App.js
@@ -22,6 +22,7 @@ class App extends React.Component {
         <h1>Tracking your Networth</h1>
         <SelectCurrency />
         <h3>Net Worth</h3>
+
         <h3>Assets</h3>
         <ItemTable
           column1header="Cash and Investments"
@@ -35,12 +36,13 @@ class App extends React.Component {
         <ItemTable
           column1header="Long Term Assets"
           column2header=""
-          column3header="Interest Rate"
+          column3header=""
           items={this.state.balanceSheet.items.filter(item => {
             return item.type.match("asset") && item.term.match("long");
           })}
           onAmountChanged={this.onAmountChanged}
         />
+
         <h3>Liabilities</h3>
         <ItemTable
           column1header="Short Term Liabilities"
@@ -48,6 +50,15 @@ class App extends React.Component {
           column3header="Interest Rate"
           items={this.state.balanceSheet.items.filter(item => {
             return item.type.match("liability") && item.term.match("short");
+          })}
+          onAmountChanged={this.onAmountChanged}
+        />
+        <ItemTable
+          column1header="Long Term Debt"
+          column2header=""
+          column3header=""
+          items={this.state.balanceSheet.items.filter(item => {
+            return item.type.match("liability") && item.term.match("long");
           })}
           onAmountChanged={this.onAmountChanged}
         />
